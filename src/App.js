b80f8f642dd62207f14e3e0c320f638d277635a1
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-04-03 08:51:03 +0000

    implement add to cart feature using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
 
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const { cartItems, isLoading } = useSelector((store) => store.cart);
+	const { isOpen } = useSelector((store) => store.modal);
+	const dispatch = useDispatch();
 
+	useEffect(() => {
+		dispatch(calculateTotals());
+	}, [cartItems, dispatch]);
+
+	useEffect(() => {
+		dispatch(getCartItems("random"));
+	}, [dispatch]);
+
+	if (isLoading) {
+		return (
+			<div className="loading">
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
+
+	return (
+		<main>
+			{isOpen && <Modal />}
+			<Navbar />
+			<CartContainer />
+		</main>
+	);
+}
 export default App;
